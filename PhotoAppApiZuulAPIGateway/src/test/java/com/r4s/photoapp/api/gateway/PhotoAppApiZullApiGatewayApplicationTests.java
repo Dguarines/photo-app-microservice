64277e17c227b4016a755277ac6321d176986e2f
Deivison <dguarines@gmail.com>
2019-06-13 13:55:30 -0300

    First Commit

---
+++ PhotoAppApiZuulAPIGateway/src/test/java/com/r4s/photoapp/api/gateway/PhotoAppApiZullApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.r4s.photoapp.api.gateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PhotoAppApiZullApiGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
