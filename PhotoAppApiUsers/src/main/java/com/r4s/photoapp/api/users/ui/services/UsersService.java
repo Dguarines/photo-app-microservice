76fd5ab7c990a7ad43850a81a935f96745bd593c
Deivison <dguarines@gmail.com>
2019-06-19 13:39:20 -0300

    Implementing Service Layer Class

---
+++ PhotoAppApiUsers/src/main/java/com/r4s/photoapp/api/users/ui/services/UsersService.java
@@ -0,0 +1,6 @@
+package com.r4s.photoapp.api.users.ui.services;
+
+public interface UsersService {
+	
+	UserDto createUser(UserDto userDetails);
+}
---
+++ PhotoAppApiUsers/src/main/java/com/r4s/photoapp/api/users/ui/services/UsersServiceImpl.java
@@ -0,0 +1,10 @@
+package com.r4s.photoapp.api.users.ui.services;
+
+public class UsersServiceImpl implements UsersService {
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		return null;
+	}
+
+}
