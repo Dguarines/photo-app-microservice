110a43cd727c87cf6262b6f3ed4eb70414728816
Deivison <dguarines@gmail.com>
2019-06-24 12:44:01 -0300

    Adding Support for Spring Data JPA

--- PhotoAppApiUsers/src/main/java/com/r4s/photoapp/api/users/ui/services/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/r4s/photoapp/api/users/ui/services/UsersServiceImpl.java
@@ -1,11 +1,16 @@
 package com.r4s.photoapp.api.users.ui.services;
 
+import java.util.UUID;
+
 import com.r4s.photoapp.api.users.shared.UserDto;
 
 public class UsersServiceImpl implements UsersService {
 
 	@Override
 	public UserDto createUser(UserDto userDetails) {
+		
+		userDetails.setUserId(UUID.randomUUID().toString());
+		
 		return null;
 	}
 
